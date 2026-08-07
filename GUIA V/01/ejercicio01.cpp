6ee95478fb91bf80b017bfc9cce36a5f74330877
snidersullcajihuallanca-source <sullcadenis957@gmail.com>
2026-08-07 11:04:11 -0500

    actualizar

---
+++ GUIA V/01/ejercicio01.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool Esprimo(int n){
+    int contadorD = 0;
+
+    if (n < 2)
+        return false;
+    
+    for (int i = 1; i <= n; i++){
+        if (n % i == 0)
+            contadorD++;
+    }
+
+    if (contadorD > 2)
+        return false;
+
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Ingresa un numero: "; cin >> n;
+
+    cout << Esprimo(n) << endl;
+    return 0;
+}
