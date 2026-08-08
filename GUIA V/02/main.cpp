a67fc3efd292d9f8dce03cfc080a50143416dc8c
snidersullcajihuallanca-source <sullcadenis957@gmail.com>
2026-08-07 20:56:08 -0500

    ya esta

--- GUIA V/02/main.cpp
+++ GUIA V/02/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int x) {
+    if (x == 0 || x == 1)
+        return 1;
+
+    return x * factorial(x - 1);
+}
+
 int main() {
-    
+    int n;
+    cout << "Ingrese un numero: "; cin >> n;
+
+    cout << factorial(n) << endl;
     return 0;
 }
